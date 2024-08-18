ce9f5ed83f456f546aba677168de4c671e501652
Your NameSollybella2018 <assilomar@hotmail.com>
2024-08-18 18:39:25 +0300

    REsolved issues with Tailwind and daisyui and flexbox.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Buttons = () => {
   return (
-    <div><button className="btn btn-outline">Default</button>
+    <div><button className="btn btn-outline ">Default</button>
     <button className="btn btn-outline btn-primary">Primary</button>
     <button className="btn btn-outline btn-secondary">Secondary</button>
     <button className="btn btn-outline btn-accent">Accent</button></div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
       </div>
       <a className="btn btn-ghost text-xl">daisyUI</a>
     </div>
-    <div className="navbar-center hidden lg:flex">
+    <div className="navbar-center hidden bg-pink-300 lg:flex">
       <ul className="menu menu-horizontal px-1">
         <li><a>Item 1</a></li>
         <li>
