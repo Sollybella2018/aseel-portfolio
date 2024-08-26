c6948f2c3b10aa98152eb6cc0ad97b666eb40c94
Your NameSollybella2018 <assilomar@hotmail.com>
2024-08-26 23:46:58 +0300

    added img

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import logo from '../assets/logo.jpg'
+import logoj from '../assets/logoj.webp'
+import logor from '../assets/logor.jpg'
+import logov from '../assets/logov.png'
 
 const Skills = props => {
   return (
@@ -9,6 +13,8 @@ const Skills = props => {
           <div className="text-center p-6">
             <h2 className="text-2xl font-bold">My multiple skills are:</h2>
             <p className="mt-4 text-lg">ReactJS</p>
+
+            <img src = {logo} className="w-20 h-30 max-w-sm rounded-lg shadow-2xl " />
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">❮</a>
@@ -19,6 +25,8 @@ const Skills = props => {
           <div className="text-center p-6">
             <h2 className="text-2xl font-bold">My multiple skills are:</h2>
             <p className="mt-4 text-lg">Javascript</p>
+            <img src = {logoj} className="w-20 h-30 max-w-sm rounded-lg shadow-2xl" />
+
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">❮</a>
@@ -29,6 +37,8 @@ const Skills = props => {
           <div className="text-center p-6">
             <h2 className="text-2xl font-bold">My multiple skills are:</h2>
             <p className="mt-4 text-lg">Responsive web design</p>
+            <img src = {logor} className="w-20 h-30 max-w-sm rounded-lg shadow-2xl" />
+
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">❮</a>
@@ -39,6 +49,8 @@ const Skills = props => {
           <div className="text-center p-6">
             <h2 className="text-2xl font-bold">My multiple skills are:</h2>
             <p className="mt-4 text-lg">VS code</p>
+            <img src = {logov} className="w-20 h-30 max-w-sm rounded-lg shadow-2xl" />
+
           </div>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">❮</a>
