66b91af87b6561cd4d778b36f045855b1a4bfe04
Your NameSollybella2018 <assilomar@hotmail.com>
2024-08-28 03:41:11 +0300

    adjust the logos

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,167 +13,58 @@ import dev from "../assets/dev.jpg";
 const Skills = (props) => {
   return (
     <div>
-      <div className="carousel w-full">
-        <div
-          id="slide1"
-          className="carousel-item relative w-full flex items-center justify-center bg-gray-100"
-        >
-          <div className="text-center p-6">
-            <h2 className="text-2xl font-bold">My multiple skills are:</h2>
-            
+<div className="carousel rounded-box">
+  <div className="carousel-item">
+    <img
+      src={logo}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src={logoj}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+     src={dev}
+     className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+     src={adobelogo}
+     className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src={logor}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src={logov}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src={htmlogo}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src={csslogo}
+      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
+      alt="Burger" />
+  </div>
+</div>
 
-            <div className="text-center grid grid-cols-4 grid-rows-2 gap-2">
-              <img
-                src={logo}
-                className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                 />
-                 </div>
-                  <div>
-                    <img
-                      src={logoj}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={dev}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={adobelogo}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce flex align-center">React JS </h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">Javascript</h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">Dev </h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">Adobe</h1>
-                  </div>
-                  </div>
-          
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div
-          id="slide2"
-          className="carousel-item relative w-full flex items-center justify-center bg-gray-100"
-        >
-          <div className="text-center p-6">
-            <h2 className="text-2xl font-bold">My multiple skills are:</h2>
-            <p className="mt-4 text-lg">Javascript</p>
-            <div className="flex justify-center">
-              <img
-                src={logoj}
-                className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-              />
-            </div>
-          </div>
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div
-          id="slide3"
-          className="carousel-item relative w-full flex items-center justify-center bg-gray-100"
-        >
-          <>
-            
-            <div
-              id="slide3"
-              className="carousel-item relative w-full flex items-center justify-center bg-gray-100"
-            >
-              <>
-                <h2 className="text-2xl font-bold flex align-center">
-                  My multiple skills are:
-                </h2>
-                <div className="text-center grid grid-cols-4 grid-rows-2 gap-2">
-                  <div>
-                    <img
-                      src={logor}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl flex align-center"
-                    />
-                  </div>
-                  
-                  
-                  <div>
-                    <img
-                      src={logov}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={htmlogo}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={csslogo}
-                      className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce flex align-center">Responsive Web Design </h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">VS Code </h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">HTML</h1>
-                  </div>
-                  <div>
-                  <h1 className ="animate-bounce">CSS</h1>
-                  </div>
-                </div>
-              </>
-            </div>
-          </>
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-          
-          </div>
-        </div>
-        
-          <div className="text-center p-6">
-            <h2 className="text-2xl font-bold">My multiple skills are:</h2>
-            <p className="mt-4 text-lg">VS code</p>
-            <img
-              src={logov}
-              className="w-20 h-30 max-w-sm rounded-lg shadow-2xl"
-            />
-          </div>
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
+      
       </div>
     
   );
